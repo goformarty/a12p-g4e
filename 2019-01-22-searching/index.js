2511e65c76ae67b95b7824aad2134eeff3716cc5
Marty Stepien <goformarty@goformarty-macbookpro.roam.corp.google.com>
2019-01-22 10:51:53 +0000

    binary search to return min, mid and max occurance

--- 2019-01-22-searching/index.js
+++ 2019-01-22-searching/index.js
@@ -1,13 +1,34 @@
+//  binary search function that returns min, mid and max occurance of searched value
+
 function binarySearch(array, value) {
-    var mid, 
+    var mid,
         low = 0,
         high = array.length - 1;
 
-    while(low <= high) {
+    while (low <= high) {
         mid = Math.floor((low + high) / 2);
-        if(array[mid] === value) {
-            return mid;
-        } else if(array[mid] < value) {
+
+        if (array[mid] === value) {
+            let min = mid;
+            let max = mid;
+
+            // find the min occurance
+            while (array[min - 1] === value) {
+                min = min - 1;
+            }
+
+            // find the max occurance
+            while((arr[max+1]) === value) {
+                max = max + 1;
+            }
+
+            return {
+                min: min,
+                mid: mid,
+                max: max
+            };
+
+        } else if (array[mid] < value) {
             low = mid + 1;
         } else {
             high = mid - 1;
@@ -17,6 +38,6 @@ function binarySearch(array, value) {
     return -1;
 }
 
-const arr = [2,5,8,9,13,45,67,99];
+const arr = [2, 5, 8, 9, 9, 9, 9, 13, 45, 67, 99];
 
-console.log(binarySearch(arr, 67));
+console.log(binarySearch(arr, 9));
